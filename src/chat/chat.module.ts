a67ad7ea681eec7a94e4791d6ab884c4e0e03261
ohshyuk5 <seunghyukoh0@gmail.com>
2021-04-01 09:28:51 +0000

    Update - 불필요한 파일 삭제 - 불필요한 기능을 제거함

--- src/chat/chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatGateway } from './chat.gateway';
-
-@Module({
-  imports: [],
-  providers: [ChatGateway],
-  exports: [],
-})
-export class ChatModule {}
